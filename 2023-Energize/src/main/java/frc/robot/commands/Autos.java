46870af7cbd504e33158a708b0e1c9a2a7910547
hackbots <3414hackbots@gmail.com>
2023-01-09 20:05:56 -0500

    removed comments

--- 2023-Energize/src/main/java/frc/robot/commands/Autos.java
+++ 2023-Energize/src/main/java/frc/robot/commands/Autos.java
@@ -1,6 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+//Delete this file
 
 package frc.robot.commands;
 
@@ -9,7 +7,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
   public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
     return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
   }
