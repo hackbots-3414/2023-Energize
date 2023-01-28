2fcbe755b38a7072982a71737dd8c792ebf07a97
vijay_n7 <vijaynannapuraju@gmail.com>
2023-01-28 13:42:16 -0500

    reset wheels

--- 2023-Energize/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Energize/src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,12 +10,6 @@ import frc.robot.commands.GyroBasedBalancing;
 import frc.robot.commands.TeleopSwerve;
 import frc.robot.subsystems.Swerve;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
   /* Controllers */
   private final Joystick driver = new Joystick(0);
@@ -38,10 +27,6 @@ public class RobotContainer {
   /* Subsystems */
   private final Swerve s_Swerve = new Swerve();
 
-
-
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     boolean fieldRelative = true;
     boolean openLoop = true;
@@ -51,12 +36,6 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
     /* Driver Buttons */
     zeroGyro.whileTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
@@ -64,14 +43,7 @@ public class RobotContainer {
     setX.whileTrue(new InstantCommand(() -> s_Swerve.setX()));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   *
-   */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
     return AutonomousFactory.getInstance(s_Swerve).testAuto();
   }
 
