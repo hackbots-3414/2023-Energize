637fa8b66f96bdeeade74ac3701bd549cf79084b
AkshayKumar41 <akskum700@gmail.com>
2023-02-24 14:57:23 -0500

    added full enums

--- 2023-Energize/src/main/java/frc/robot/autos/AutonomousFactory.java
+++ 2023-Energize/src/main/java/frc/robot/autos/AutonomousFactory.java
@@ -7,7 +7,6 @@ package frc.robot.autos;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
@@ -35,35 +34,60 @@ public class AutonomousFactory {
     public int choice;
 
     public enum AutonChoice {
-        // BottomObjectOne(),
-        // BottomObjectTwo(),
-        // BottomObjectThree(),
-        // BottomObjectFour(),
-        // BottomObjectFive(),
-        // BottomObjectSix(),
-        // BottomObjectSeven(),
-        // BottomObjectEight(),
-        // TopObjectOne(),
-        // TopObjectTwo(),
-        // TopObjectThree(),
-        // TopObjectFour(),
-        // TopObjectFive(),
-        // TopObjectSix(),
-        // TopObjectSeven(),
-        // TopObjectEight(),
+        BottomObjectOne("Bottom Object One"),
+        BottomObjectTwo("Bottom Object Two"),
+        BottomObjectThree("Bottom Object Three"),
+        BottomObjectFour("Bottom Object Four"),
+        BottomObjectFive("Bottom Object Five"),
+        BottomObjectSix("Bottom Object Six"),
+        BottomObjectSeven("Bottom Object Seven"),
+        BottomObjectEight("Bottom Object Eight"),
+        TopObjectOne("Top Object One"),
+        TopObjectTwo("Top Object Two"),
+        TopObjectThree("Top Object Three"),
+        TopObjectFour("Top Object Four"),
+        TopObjectFive("Top Object Five"),
+        TopObjectSix("Top Object Six"),
+        TopObjectSeven("Top Object Seven"),
+        TopObjectEight("Top Object Eight"),
         AutoBalance("Balance"), 
-        // TopStart(), 
-        // MidStartTop(),
-        // MidStartLow(),
-        // LowStart(),
+        TopStart("Top Start"), 
+        MidStartTop("Mid Start Top"),
+        MidStartLow("Mid Start Low"),
+        LowStart("Low Start"),
         DriveOutTop("Drive Out Top"),
-        DriveOutLow("Drive Out Low");
+        DriveOutLow("Drive Out Low"),
+        DriveStraight("DriveStraight");
 
         public final String value;
 
         AutonChoice(String value) {
             this.value = value;
         }
+    }
+
+    public enum Bays {
+        LowLow("Bay Low Low"),
+        LowMid("Bay Low Mid"),
+        LowHigh("Bay Low High"),
+        MidLow("Bay Mid Low"),
+        MidMid("Bay Mid Mid"),
+        MidHigh("Bay Mid High"),
+        HighLow("Bay High Low"),
+        HighMid("Bay High Mid"),
+        HighHigh("Bay High High");
+
+        public final String value;
+
+        Bays(String value) {
+            this.value = value;
+        }
+    }
+
+    public enum Heights {
+        Low,
+        Mid,
+        High;
     }
 
     private static final AutonomousFactory me = new AutonomousFactory();
@@ -101,55 +125,49 @@ public class AutonomousFactory {
         return new PathPoint(pose.getTranslation(), pose.getRotation());
     }
 
-    // private Command followTrajectoryOnTheFly(PathPoint... pathPoints){
-    //     ArrayList<PathPoint> points = new ArrayList<>();
-    //     for (PathPoint pathPoint : pathPoints) {
-    //         points.add(pathPoint);
-    //     }
+    private Command followTrajectoryOnTheFly(PathPoint... pathPoints){
+        ArrayList<PathPoint> points = new ArrayList<>();
+        for (PathPoint pathPoint : pathPoints) {
+            points.add(pathPoint);
+        }
 
-    //     PathPlannerTrajectory traj = PathPlanner.generatePath(new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared), points);
+        PathPlannerTrajectory traj = PathPlanner.generatePath(new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared), points);
 
-    //     return new PPSwerveControllerCommand(
-    //         traj, 
-    //         swerve::getPose, // Pose supplier
-    //         Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
-    //         new PIDController(Constants.AutoConstants.kPXController, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-    //         new PIDController(Constants.AutoConstants.kPYController, 0, 0), // Y controller (usually the same values as X controller)
-    //         new PIDController(Constants.AutoConstants.kPThetaController, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-    //         swerve::setModuleStates, // Module states consumer
-    //         true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-    //         swerve // Requires this drive subsystem
-    //     );
-    // }
+        return new PPSwerveControllerCommand(
+            traj, 
+            swerve::getPose, // Pose supplier
+            Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
+            new PIDController(Constants.AutoConstants.kPXController, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+            new PIDController(Constants.AutoConstants.kPYController, 0, 0), // Y controller (usually the same values as X controller)
+            new PIDController(Constants.AutoConstants.kPThetaController, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+            swerve::setModuleStates, // Module states consumer
+            true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+            swerve // Requires this drive subsystem
+        );
+    }
 
-    // private Command followTrajectoryCommand(String pathName, boolean isFirstPath) {
-    //     PathPlannerTrajectory traj = PathPlanner.loadPath(pathName, new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
-    //     return new SequentialCommandGroup(
-    //          new InstantCommand(() -> {
-    //            // Reset odometry for the first path you run during auto
-    //            if(isFirstPath){
-    //                swerve.resetOdometry(traj.getInitialHolonomicPose());
-    //            }
-    //          }),
-    //          new PPSwerveControllerCommand(
-    //              traj, 
-    //              swerve::getPose, // Pose supplier
-    //              Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
-    //              new PIDController(Constants.AutoConstants.kPXController, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-    //              new PIDController(Constants.AutoConstants.kPYController, 0, 0), // Y controller (usually the same values as X controller)
-    //              new PIDController(Constants.AutoConstants.kPThetaController, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
-    //              swerve::setModuleStates, // Module states consumer
-    //              false, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
-    //              swerve // Requires this drive subsystem
-    //          )
-    //     );
-    // }
-
-    // private Command followTrajectoryWithEventsAndOnTheFlyCommand(String pathName) {
-    //     ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup(pathName, new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
-
-    //     return new SequentialCommandGroup(followTrajectoryOnTheFly(resetToVision(), new PathPoint(pathGroup.get(0).getInitialPose().getTranslation(), pathGroup.get(0).getInitialPose().getRotation())), autoBuilder.fullAuto(pathGroup));
-    // }
+    private Command followTrajectoryCommand(String pathName, boolean isFirstPath) {
+        PathPlannerTrajectory traj = PathPlanner.loadPath(pathName, new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
+        return new SequentialCommandGroup(
+             new InstantCommand(() -> {
+               // Reset odometry for the first path you run during auto
+               if(isFirstPath){
+                   swerve.resetOdometry(traj.getInitialHolonomicPose());
+               }
+             }),
+             new PPSwerveControllerCommand(
+                 traj, 
+                 swerve::getPose, // Pose supplier
+                 Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
+                 new PIDController(Constants.AutoConstants.kPXController, 0, 0), // X controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+                 new PIDController(Constants.AutoConstants.kPYController, 0, 0), // Y controller (usually the same values as X controller)
+                 new PIDController(Constants.AutoConstants.kPThetaController, 0, 0), // Rotation controller. Tune these values for your robot. Leaving them 0 will only use feedforwards.
+                 swerve::setModuleStates, // Module states consumer
+                 false, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+                 swerve // Requires this drive subsystem
+             )
+        );
+    }
 
     private Command followTrajectoryWithEventsCommand(String pathName) {
         ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup(pathName, new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
@@ -157,12 +175,31 @@ public class AutonomousFactory {
         return autoBuilder.fullAuto(pathGroup);
     }
 
-    public Command eventChooser(AutonChoice choice) {
-        // return followTrajectoryWithEventsCommand("Drive Out Top");
-        return followTrajectoryWithEventsCommand(choice.value);
+    private Command followTrajectoryWithEventsAndOnTheFlyCommand(String pathName) {
+        ArrayList<PathPlannerTrajectory> pathGroup = (ArrayList<PathPlannerTrajectory>) PathPlanner.loadPathGroup(pathName, new PathConstraints(Constants.AutoConstants.kMaxSpeedMetersPerSecond, Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared));
+
+        return new SequentialCommandGroup(followTrajectoryOnTheFly(resetToVision(), new PathPoint(pathGroup.get(0).getInitialPose().getTranslation(), pathGroup.get(0).getInitialPose().getRotation())), autoBuilder.fullAuto(pathGroup));
+    }
+
+    private Command placeCommand(Heights height) {
+        if (height == Heights.Low) {
+            // code to place object low
+        } else if (height == Heights.Mid) {
+            // code to place object mid
+        } else if (height == Heights.High) {
+            // code to place objects high
+        }
+        return null;
+    }
+
+    public Command eventChooser(AutonChoice choice, Bays bay, Heights height) { // probably should implement a boolean field about whether balance, and add a command
+        return new SequentialCommandGroup(followTrajectoryWithEventsCommand(choice.value), followTrajectoryWithEventsCommand(bay.value), placeCommand(height));
+        // return followTrajectoryWithEventsCommand(choice.value);
+        // ^^ testing purposes
     }
 
     public Command autobalance() {
+        // just so that we can do an auto balance auton if we ever need to
         return new SequentialCommandGroup(followTrajectoryWithEventsCommand(AutonChoice.AutoBalance.value), new PIDBalance(swerve));
     }
 }
