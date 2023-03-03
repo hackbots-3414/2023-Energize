0f67629056ed2bab10bf0bc3dd79c4d9af279c0b
vijay_n7 <vijaynannapuraju@gmail.com>
2023-03-03 08:39:31 -0500

    working

--- 2023-Energize/src/main/java/frc/robot/autos/AutonomousFactory.java
+++ 2023-Energize/src/main/java/frc/robot/autos/AutonomousFactory.java
@@ -35,7 +35,7 @@ public class AutonomousFactory {
     public int choice;
 
     public enum AutonChoice {
-        Balance("Balance"),
+        Balance("Mid Balance"),
         Left("Left"),
         Right("Right"),
         Nothing("Nothing");
