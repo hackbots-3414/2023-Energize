6e5a5d04e29aefeafd9d95edbc9f3c17e08fc2c6
Adriaan Friesen <adriaan.friesen@gmail.com>
2023-03-11 13:17:51 -0500

    Path Factory works now!

--- 2023-Energize/src/main/java/frc/lib/pathFactory/PathFactory.java
+++ 2023-Energize/src/main/java/frc/lib/pathFactory/PathFactory.java
@@ -1,157 +1,103 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-//TODO: Hello, we noticed that some of these variables were named in a way which made it difficult to understand their meanings.  Because we didn't understand the function, we took the liberty of pointing them out using the Todo comments so we can re-name them in the future.
 package frc.lib.pathFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.TreeMap;
 
-//import javax.swing.text.Position;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 import frc.robot.Constants;
-import frc.robot.SwerveModule;
-//import frc.robot.Constants.Swerve;
-import frc.robot.subsystems.Swerve;
 
 /** Add your docs here. */
 public class PathFactory {
     private static PathFactory me = new PathFactory();
 
-    // I made these public because they should be used when using the getPath() method.
-    // TODO: Put in values for the positions of the things. Also ensure that you switch the side depending on the side of the field we're on.
+    private boolean isRedSide = (DriverStation.getAlliance() == Alliance.Red); // If driver station is NOT open, it will evaluate to true for some reason.
 
-    private boolean isRedSide = true;
     private double x = (isRedSide) ? Constants.PathFactory.redSide.x : Constants.PathFactory.blueSide.x;
 
-    private double abx = (isRedSide) ? Constants.PathFactory.redSide.abx : Constants.PathFactory.blueSide.abx;
     private double cx = (isRedSide) ? Constants.PathFactory.redSide.cx : Constants.PathFactory.blueSide.cx;
 
-    public Pose2d p1 = new Pose2d(new Translation2d(x, Constants.PathFactory.p1), new Rotation2d());
-    public Pose2d p2 = new Pose2d(new Translation2d(x, Constants.PathFactory.p2), new Rotation2d());
-    public Pose2d p3 = new Pose2d(new Translation2d(x, Constants.PathFactory.p3), new Rotation2d());
-    public Pose2d p4 = new Pose2d(new Translation2d(x, Constants.PathFactory.p4), new Rotation2d());
-    public Pose2d p5 = new Pose2d(new Translation2d(x, Constants.PathFactory.p5), new Rotation2d());
-    public Pose2d p6 = new Pose2d(new Translation2d(x, Constants.PathFactory.p6), new Rotation2d());
-    public Pose2d p7 = new Pose2d(new Translation2d(x, Constants.PathFactory.p7), new Rotation2d());
-    public Pose2d p8 = new Pose2d(new Translation2d(x, Constants.PathFactory.p8), new Rotation2d());
-    public Pose2d p9 = new Pose2d(new Translation2d(x, Constants.PathFactory.p9), new Rotation2d());
+    private Pose2d p1 = new Pose2d(new Translation2d(x, Constants.PathFactory.p1), new Rotation2d());
+    private Pose2d p2 = new Pose2d(new Translation2d(x, Constants.PathFactory.p2), new Rotation2d());
+    private Pose2d p3 = new Pose2d(new Translation2d(x, Constants.PathFactory.p3), new Rotation2d());
+    private Pose2d p4 = new Pose2d(new Translation2d(x, Constants.PathFactory.p4), new Rotation2d());
+    private Pose2d p5 = new Pose2d(new Translation2d(x, Constants.PathFactory.p5), new Rotation2d());
+    private Pose2d p6 = new Pose2d(new Translation2d(x, Constants.PathFactory.p6), new Rotation2d());
+    private Pose2d p7 = new Pose2d(new Translation2d(x, Constants.PathFactory.p7), new Rotation2d());
+    private Pose2d p8 = new Pose2d(new Translation2d(x, Constants.PathFactory.p8), new Rotation2d());
+    private Pose2d p9 = new Pose2d(new Translation2d(x, Constants.PathFactory.p9), new Rotation2d());
 
-    public Pose2d pA = new Pose2d(new Translation2d(abx, Constants.PathFactory.a), new Rotation2d());
-    public Pose2d pB = new Pose2d(new Translation2d(abx, Constants.PathFactory.b), new Rotation2d());
-    public Pose2d pC1 = new Pose2d(new Translation2d(/*PUT VALUES HERE*/cx, Constants.PathFactory.c1), new Rotation2d());
-    public Pose2d pC2 = new Pose2d(new Translation2d(/*PUT VALUES HERE*/cx, Constants.PathFactory.c2), new Rotation2d());
+    private Pose2d pC1 = new Pose2d(new Translation2d(cx, Constants.PathFactory.c1), new Rotation2d());
+    private Pose2d pC2 = new Pose2d(new Translation2d(cx, Constants.PathFactory.c2), new Rotation2d());
 
-    private List<Pose2d> poses = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, pA, pB, pC1, pC2);
-    //FIXME: Put pA and pB inside the lists so we can drive into the entrance after hitting C1 and C2
-    private List<Pose2d> pA_path = Arrays.asList(pC2, p9, p8, p7, p6, p5, p4, p3, p2, p1);
-    private List<Pose2d> pB_path = Arrays.asList(pC1, p1, p2, p3, p4, p5, p6, p7, p8, p9);
-    private List<Pose2d> pC1_path = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9);
-    private List<Pose2d> pC2_path = Arrays.asList(p9, p8, p7, p6, p5, p4, p3, p2, p1);
+    private List<Pose2d> scoring_points = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9);
+  
+    int pA_INDEX = 0;
+    int pB_INDEX = 1;
+    int pC1_INDEX = 2;
+    int pC2_INDEX = 3;
 
-    //FIXME: TreeMap doesn't work when using a Pose2d key.  We need a different way to order the lists of poses.
-
-    private TreeMap<Pose2d, List<Pose2d>> paths = new TreeMap<Pose2d, List<Pose2d>>();
-
-    private PathFactory() {
-        try {
-        System.out.println(pA_path instanceof List<?>);
-        paths.put(pA, pA_path);
-        paths.put(pB, pB_path);
-        paths.put(pC1, pC1_path);
-        paths.put(pC2, pC2_path);            
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-            throw e;
-        }
-
-    }
+    private PathFactory() {}
 
     public static PathFactory getInstance() {
         return me;
     }
-    //TODO: Rename variables 'a' and 'b' for more clarity to the teammates working on the code
     private double getDistance(Pose2d a, Pose2d b) {
         return a.getTranslation().getDistance(b.getTranslation());
-        // Pose2d relative = a.relativeTo(b);
-        // double distance = Math.sqrt(
-        //     Math.pow(relative.getX(), 2)
-        //     +
-        //     Math.pow(relative.getY(), 2)
-        // );
-        // return distance;
     }
 
     public List<Pose2d> getPath(Pose2d from, int toInt) {
-        //TODO: Rename the variable 'to' for better clarity
-        //TODO: 'figure out which scoring point to go to' could be taken as "We need to do this" or "This line of code does this"
-        Pose2d to = pA_path.get(toInt); // figure out which scoring point to go to
+        Pose2d target = scoring_points.get(toInt - 1); // figure out which scoring point to go to (this does that)
 
-        
-        //TODO: Which point list?  Options are pA, pB, pC1, pC2.
-        // add all of the points in between:
-        /*
-        TODO:    a. loop through all of the points in the point list
-        TODO:    b. find the point that would bring us closest to the destination
-        TODO:    c. Find that point's path.
-        TODO:    d. Add each point in the path.
-        */
+        double targetX = target.getX();
 
-        //TODO: This best_start_pose variable in the line below is never set as anything other than 'poses.get(0)'.  Ask Nolan what best_start_pose is supposed to do, then effectively set it as what it is supposed to be, as his comment suggests that best_start_pose is not supposed to stay at pose.get(0)
-        Pose2d best_start_pose = poses.get(0); // It has to be initialized with something
-
-        double currentDistanceToTarget = getDistance(from, to);
-
-        double currentBestDistanceToNextWaypoint = getDistance(from, best_start_pose);
-        //TODO: Work through this logic as a team
-        for (int i = 0;i < poses.size();i ++) {
-            /*
-             * Find the point that is closest to the robot currently and DOES NOT take us farther away (using currentDistance)
-             */
-
-            Pose2d thisPose = poses.get(i);
-
-            if (getDistance(from, thisPose) < currentBestDistanceToNextWaypoint) {
-                if (getDistance(thisPose, to) < currentDistanceToTarget) {
-                    best_start_pose = thisPose;
-                    currentBestDistanceToNextWaypoint = getDistance(from, thisPose);
-                }
-            }
-
-            
-        }
+        double outsideBarrier = pC1.getX();
 
         boolean outside = false;
 
-        if (best_start_pose == pA || best_start_pose == pB || best_start_pose == pC1 || best_start_pose == pC2) {
+        if (isRedSide && from.getX() < outsideBarrier) {
+            outside = true;
+        } else if (!isRedSide && from.getX() > outsideBarrier) {
             outside = true;
         }
 
-        if (!outside) {
-            return Arrays.asList(to, from);
-        }
+        List<Pose2d> result = new ArrayList<Pose2d>();
 
-        List<Pose2d> path = paths.get(best_start_pose);
+        result.add(from);
+        Pose2d closer;
 
-        ArrayList<Pose2d> results = new ArrayList<Pose2d>();
-        results.add(from);
-        //TODO:  What does this logic do?
-        for (int i = 0;i < path.size();i++) {
-            if (path.get(i) != to) {
-                results.add(path.get(i));
+        if (outside) {
+            // which is closer: C1 or C2:
+            double distanceToC1 = getDistance(from, pC1);
+            double distanceToC2 = getDistance(from, pC2);
+
+
+            if (distanceToC1 > distanceToC2) {
+                closer = pC2;
             } else {
-                break;
+                closer = pC1;
             }
-        }
-        //TODO: What is the meaning of the variable 'to'?  Can we name it better?
-        results.add(to);
 
-        return results;
+            result.add(closer);
+        } else {
+            closer = from;
+        }
+
+        Pose2d nextPoint = new Pose2d(new Translation2d(targetX, closer.getY()), new Rotation2d());
+
+        result.add(nextPoint);
+
+        result.add(target);
+
+        return result;
     }
 
 }
--- 2023-Energize/src/test/java/frc/robot/lib/pathFactory/PathFactoryTest.java
+++ 2023-Energize/src/test/java/frc/robot/lib/pathFactory/PathFactoryTest.java
@@ -5,23 +5,48 @@
 package frc.robot.lib.pathFactory;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.lib.pathFactory.PathFactory;
 
 /** Add your docs here. */
 public class PathFactoryTest {
     @Test
-    public void testPathFactoryConstructor(){
+    public void testPathFactoryConstructor() {
         try {
         assertNotNull(PathFactory.getInstance(), "Got PathFactory instance");            
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    @Test
+    public void testPathFactoryAgain() {
+        try {
+
+            PathFactory instance = PathFactory.getInstance();
+
+            Translation2d translation = new Translation2d(500, 40);
+            Rotation2d rotation = new Rotation2d();
+
+            List<Pose2d> path = instance.getPath(new Pose2d(translation, rotation), 9);
+
+            for (int i = 0;i < path.size();i ++ ) {
+                System.out.println(i + ": " + path.get(i));
+            }
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
     }
 
 }
