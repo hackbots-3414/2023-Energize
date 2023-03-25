0c0f0b112aca73b3ab53e1508eb0156fb2326fcc
HeidiG2007 <78167256+HeidiG2007@users.noreply.github.com>
2023-03-25 09:36:51 -0400

    Fix todo

--- 2023-Energize/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Energize/src/main/java/frc/robot/RobotContainer.java
@@ -49,11 +49,7 @@ public class RobotContainer {
   // XboxController.Button.kA.value);
   // private final JoystickButton setX = new JoystickButton(driver,
   // XboxController.Button.kX.value);
-  private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value); // fix
-                                                                                                                   // me
-                                                                                                                   // Swerve
-                                                                                                                   // subsys
-                                                                                                                   // overwrites
+  private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value); // FIXME: Swerve Subsys overwrites
   // private final JoystickButton ledConeButton = new JoystickButton(driver, 2);
   // private final JoystickButton ledCubeButton = new JoystickButton(driver, 3);
 
