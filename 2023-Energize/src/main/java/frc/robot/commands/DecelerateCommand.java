490b8bf215051d45e8ae2f4ab2c0b5836c08432a
Nolan Barker <nolanb414@yahoo.com>
2023-04-03 17:59:50 -0400

    Update DecellerateCommand.java with main

--- 2023-Energize/src/main/java/frc/robot/commands/DecelerateCommand.java
+++ 2023-Energize/src/main/java/frc/robot/commands/DecelerateCommand.java
@@ -36,14 +36,8 @@ public class DecelerateCommand extends CommandBase {
     this.strafeSup = strafeSup;
     this.rotationSup = rotationSup;
     this.robotCentricSup = robotCentricSup;
-    // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     Pose2d currentPose = swerve.getPose();
@@ -84,11 +78,6 @@ public class DecelerateCommand extends CommandBase {
 
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return irSensor.getIRState();
